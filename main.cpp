b7bc4eff86889494a5d1287d42caee6dcc70f1a6
dandsager <dandsager@localhost.localdomain>
2019-12-05 23:26:51 -0500

    installed eclipse & wrote hello world

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Donut-Chess.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World - from eclipse!!!");
+	return EXIT_SUCCESS;
+}
